30be22b6387565530a81f04a219cfeb15d05573f
Matheus Lourenço <matheus503487@gmail.com>
2022-02-21 21:22:24 -0300

    produtos style

--- src/components/ProdutosLista.vue
+++ src/components/ProdutosLista.vue
@@ -2,6 +2,10 @@
   <section>
       <div class="produtos" v-for="(produto,index) in produtos" :key="index">
           <h2>{{produto.nome}}</h2>
+          <img :src="produto.img" alt="">
+          <span>{{produto.preco | numeroPreco}}</span>
+          <p>{{produto.descricao}}</p>
+          <button class="btn">Comprar</button>
       </div>
   </section>
 </template>
@@ -11,6 +15,12 @@ export default {
     data(){
         return{
             produtos:null
+        }
+    },
+    filters:{
+        numeroPreco(currentValue){
+            parseInt(currentValue)
+            return currentValue.toLocaleString('pt-BR', {style: 'currency', currency: 'BRL'});
         }
     },
     methods:{
@@ -29,6 +39,7 @@ export default {
 <style scoped>
 
 section{
+    margin: auto;
     margin-top: 40px;
     display: flex;
     flex-wrap: wrap;
@@ -37,11 +48,30 @@ section{
 
 .produtos{
     display: flex;
-    justify-content: center;
+    flex-direction: column;
+    align-items: center;
     width: 100%;
     max-width: 300px;
     height: 400px;
     border: 2px solid #000;
+    padding: 10px;
+}
+
+.produtos h2{
+    font-size: 1.7rem;
+}
+
+.produtos img{
+    margin: 5px 0;
+    height: 50%;
+}
+
+.produtos span{
+    font-size: 1.5rem;
+}
+
+.produtos p{
+    margin: 10px 0;
 }
 
 </style>
